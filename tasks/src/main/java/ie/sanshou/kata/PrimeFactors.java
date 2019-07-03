cf7b705c1e3ee428e394644b062a9c0169ebc0ff
Tao Wei <tao.wei@paddypowerbetfair.com>
2019-07-03 19:55:07 +0100

    Refactor-6-1

--- tasks/src/main/java/ie/sanshou/kata/PrimeFactors.java
+++ tasks/src/main/java/ie/sanshou/kata/PrimeFactors.java
@@ -8,14 +8,12 @@ class PrimeFactors {
     List<Integer> primeFactorsOf(int n) {
         final ArrayList<Integer> factors = new ArrayList<>();
 
-        if (n > 1) {
-            for (;n % 2 == 0;n /= 2) 
-                factors.add(2);
-            for (;n % 3 == 0;n /= 3)
-                factors.add(3);
+        int divider = 2;
+        while (n > 1) {
+            for (; n % divider == 0; n /= divider)
+                factors.add(divider);
+            divider++;
         }
-        if (n > 1)
-            factors.add(n);
 
         return factors;
     }
