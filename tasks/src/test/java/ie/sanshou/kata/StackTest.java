70ca7c0a31d2ba1e3698690dd8ed0805d38d5e8f
Tao Wei <tao.wei@paddypowerbetfair.com>
2019-07-04 12:01:40 +0100

    TDD-1

--- tasks/src/test/java/ie/sanshou/kata/StackTest.java
+++ tasks/src/test/java/ie/sanshou/kata/StackTest.java
@@ -2,12 +2,10 @@ package ie.sanshou.kata;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class StackTest {
 
     @Test
-    public void nothing(){
-        fail();
+    public void createStack() {
+        Stack stack = new Stack();
     }
 }
