aaa3c9850b748c53290a735a65f1ac61122918bb
Tao Wei <tao.wei@paddypowerbetfair.com>
2019-07-04 12:34:13 +0100

    Refactor for 5

--- tasks/src/main/java/ie/sanshou/kata/Stack.java
+++ tasks/src/main/java/ie/sanshou/kata/Stack.java
@@ -4,6 +4,14 @@ class Stack {
 
     private int size;
 
+    static Stack make(int capacity) {
+        return new Stack(capacity);
+    }
+
+    private Stack(int capacity) {
+
+    }
+
     boolean isEmpty() {
         return size == 0;
     }
@@ -19,4 +27,5 @@ class Stack {
     void pop() {
         size--;
     }
+
 }
--- tasks/src/test/java/ie/sanshou/kata/StackTest.java
+++ tasks/src/test/java/ie/sanshou/kata/StackTest.java
@@ -1,12 +1,19 @@
 package ie.sanshou.kata;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
 
-    private Stack stack = new Stack();
+    private Stack stack;
+
+    @BeforeEach
+     void setup(){
+        stack = Stack.make(2);
+    }
 
     @Test
     void newlyCreateStack_ShouldBeEmpty() {
@@ -26,5 +33,10 @@ class StackTest {
         stack.push(1);
         stack.pop();
         assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void whenPushedPastLimit_StackOverflows(){
+        
     }
 }
