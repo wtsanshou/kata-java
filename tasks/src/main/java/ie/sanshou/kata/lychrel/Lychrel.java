7526ae4819d1b5f76ac3639f7e468217122c087a
Tao Wei <tao.wei@paddypowerbetfair.com>
2019-07-18 13:21:21 +0100

    TDD-4

--- tasks/src/main/java/ie/sanshou/kata/lychrel/Lychrel.java
+++ tasks/src/main/java/ie/sanshou/kata/lychrel/Lychrel.java
@@ -2,9 +2,14 @@ package ie.sanshou.kata.lychrel;
 
 public class Lychrel {
     public static int convergesAtIteration(int n, int limit) {
-        if (n < 9)
+        if (isPalindrome(n))
             return 0;
         else
             return 1;
+    }
+
+    public static boolean isPalindrome(int n) {
+        String digits = Integer.toString(n);
+        return digits.charAt(0) == digits.charAt(digits.length()-1);
     }
 }
--- tasks/src/test/java/ie/sanshou/kata/lychrel/LychrelTest.java
+++ tasks/src/test/java/ie/sanshou/kata/lychrel/LychrelTest.java
@@ -1,21 +1,36 @@
 package ie.sanshou.kata.lychrel;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LychrelTest {
 
     private final int LIMIT = 1000;
 
+    @Disabled
     @Test
     public void facts() {
         convergesAtIteration(1, 0);
         convergesAtIteration(2, 0);
         convergesAtIteration(10, 1);
+        convergesAtIteration(11, 0);
     }
 
     private void convergesAtIteration(int n, int iteration) {
         assertEquals(iteration, Lychrel.convergesAtIteration(n, LIMIT));
+    }
+
+    @Test
+    public void palindromes() {
+        isPalindrome(1);
+        isPalindrome(11);
+        isPalindrome(121);
+    }
+
+    private void isPalindrome(int n) {
+        assertTrue(Lychrel.isPalindrome(n));
     }
 }
