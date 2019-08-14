c6ae7b4c0fc1fc3a7e5de8dc5d925876315657a9
Tao Wei <tao.wei@paddypowerbetfair.com>
2019-08-14 18:23:11 +0100

    Refactor_4-1

--- tasks/src/test/java/ie/sanshou/kata/trie/TrieTest.java
+++ tasks/src/test/java/ie/sanshou/kata/trie/TrieTest.java
@@ -1,5 +1,6 @@
 package ie.sanshou.kata.trie;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,35 +8,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TrieTest {
 
-    @Test
-    public void trieCanInsertOneWord() {
-        Trie trie = new Trie();
-        trie.insert("test");
-        assertTrue(trie.search("test"));
+    private static Trie trie;
+
+    @BeforeAll
+    public static void setup() {
+        trie = new Trie();
+        trie.insert("test1");
+        trie.insert("test2");
     }
 
     @Test
     public void trieCanInsertTwoWord() {
-        Trie trie = new Trie();
-        trie.insert("test1");
-        trie.insert("test2");
         assertTrue(trie.search("test1"));
         assertTrue(trie.search("test2"));
     }
 
     @Test
     public void trieShouldNotFindWordsWhichAreNotInsertedIntoTheTrie() {
-        Trie trie = new Trie();
-        trie.insert("test1");
-        trie.insert("test2");
         assertFalse(trie.search("test"));
     }
 
     @Test
     public void trieCanSearchPrefix() {
-        Trie trie = new Trie();
-        trie.insert("test1");
-        trie.insert("test2");
         assertTrue(trie.startsWith("test"));
         assertFalse(trie.startsWith("tset"));
     }
