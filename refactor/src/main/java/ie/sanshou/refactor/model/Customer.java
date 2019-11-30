9a4ea7c06d28c07cf8f91d7a8f86986c3f60e8c3
Tao Wei <taowe@amazon.com>
2019-11-30 15:58:20 +0000

    Refactor-3 Move Method

--- refactor/src/main/java/ie/sanshou/refactor/model/Customer.java
+++ refactor/src/main/java/ie/sanshou/refactor/model/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
 
         for (Rental each : rentals) {
             // determine amounts for each line
-            double thisAmount = amountFor(each);
+            double thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -43,24 +43,5 @@ public class Customer {
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
     }
-
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
+    
 }
