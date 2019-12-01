cbeb08b17b69624c4da7bc9ed42c4c16ddeb154f
Tao Wei <taowe@amazon.com>
2019-12-01 12:47:41 +0000

    Refactor-11 Replace Children Price with Polymorphism

--- refactor/src/main/java/ie/sanshou/refactor/model/Price.java
+++ refactor/src/main/java/ie/sanshou/refactor/model/Price.java
@@ -3,17 +3,7 @@ package ie.sanshou.refactor.model;
 public abstract class Price {
     abstract int getPriceCode();
 
-    public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
 
 class RegularPrice extends Price {
@@ -25,7 +15,7 @@ class RegularPrice extends Price {
 
     @Override
     public double getCharge(int daysRented) {
-        return (daysRented > 2) ? (daysRented - 2) * 1.5 + 2 : 2;
+        return 2 + ((daysRented > 2) ? (daysRented - 2) * 1.5 : 0);
     }
 }
 
@@ -47,5 +37,10 @@ class ChildrenPrice extends Price {
     @Override
     int getPriceCode() {
         return Movie.CHILDREN;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return 1.5 + ((daysRented > 3) ? (daysRented - 3) * 1.5 : 0);
     }
 }
